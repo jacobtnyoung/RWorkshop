ebd171bf5ba4d0467c5858aaf63ac4724713d98e
jacobyoung20 <39632608+jacobtnyoung@users.noreply.github.com>
2022-07-21 14:17:44 -0700

    Major edits for the 2022 workshop

--- RWorkshop_AUTOMATOR_script.R
+++ RWorkshop_AUTOMATOR_script.R
@@ -1,18 +1,14 @@
 require( knitr )
 require( rmarkdown )
-#library( mailR )
 
 # Creates the html output.
 render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/index.Rmd", "html_document" )
-
-!!!keep updating these
-
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_trends.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_types.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_types_2020.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHX_heat_shelter_2020.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_GEO.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_police_shootings.Rmd", "html_document" )
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Basics.Rmd", "html_document" )
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Working_with_Data.Rmd", "html_document" )
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Programming.Rmd", "html_document" )
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Data_Visualization.Rmd", "html_document" )
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Data_Example.Rmd", "html_document" )
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/RWorkshop/R_Markdown.Rmd", "html_document" )
 
 # Close this script.
 quit( save = "no" )
