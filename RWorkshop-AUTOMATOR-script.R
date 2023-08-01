6cbd11f477b9b67136f2ecaade9231c3ab5eac9a
jacobtnyoung <CRJ605@asu.edu>
2023-08-01 09:57:04 -0700

    changed file locations and added here() function use

---
+++ RWorkshop-AUTOMATOR-script.R
@@ -0,0 +1,19 @@
+require( knitr )
+require( rmarkdown )
+require( here )
+
+# Creates the html output
+render( here( "index.Rmd" ), "html_document" )
+
+# Note that these are in the /topics folder
+render( here( "topics/R-basics.Rmd" )            , "html_document" )
+render( here( "topics/R-working-with-data.Rmd" ) , "html_document" )
+render( here( "topics/R-programming.Rmd" )       , "html_document" )
+render( here( "topics/R-data-visualization.Rmd" ), "html_document" )
+render( here( "topics/R-data-example.Rmd" )      , "html_document" )
+render( here( "topics/R-markdown.Rmd" )          , "html_document" )
+render( here( "topics/R-shiny.Rmd" )             , "html_document" )
+render( here( "topics/R-schedule.Rmd" )          , "html_document" )
+
+# Close this script.
+quit( save = "no" )
