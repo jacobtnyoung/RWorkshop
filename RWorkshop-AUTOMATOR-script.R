c726e2f8aaab96c173e837dfedc4acd0b803d7f3
jacobtnyoung <CRJ605@asu.edu>
2024-08-13 12:07:04 -0700

    major revisions for 2024 workshop

--- RWorkshop-AUTOMATOR-script.R
+++ RWorkshop-AUTOMATOR-script.R
@@ -10,9 +10,9 @@ render( here( "topics/R-basics.Rmd" )            , "html_document" )
 render( here( "topics/R-working-with-data.Rmd" ) , "html_document" )
 render( here( "topics/R-programming.Rmd" )       , "html_document" )
 render( here( "topics/R-data-visualization.Rmd" ), "html_document" )
-render( here( "topics/R-data-example.Rmd" )      , "html_document" )
 render( here( "topics/R-markdown.Rmd" )          , "html_document" )
 render( here( "topics/R-shiny.Rmd" )             , "html_document" )
+render( here( "topics/R-AI.Rmd" )                , "html_document" )
 render( here( "topics/R-schedule.Rmd" )          , "html_document" )
 
 # Close this script.
